f2aaed2618f08841de7c058a7e2a0c20b6069e2d
gritten07 <161456871+gritten07@users.noreply.github.com>
2024-10-01 08:53:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Em que continente fica o Egito?',
+    'África'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o maior planeta do sistema solar?',
+    'Júpiter'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o elemento químico com o símbolo O?',
+    'Oxigênio'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu Dom Casmuro?',
+    'Machado de Assis'
+)
